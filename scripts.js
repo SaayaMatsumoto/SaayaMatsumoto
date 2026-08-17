74c3f92e26d6a3a64815e3899dc3ca9e3b7f5afe
yutakukita <127103166+yutakukita@users.noreply.github.com>
2026-08-17 11:23:36 +0100

    Clean up parent site HTML CSS and JavaScript

--- scripts.js
+++ scripts.js
@@ -1,91 +1,18 @@
-// JavaScript Document
+/* Scroll reveal animations */
+jQuery(function ($) {
+  $(window).on('scroll load', function () {
+    var scrollTop = $(window).scrollTop();
+    var windowHeight = $(window).height();
 
-//--- sns-link-show ---//
-jQuery(function($){
-	// execute when scrolling and loading
-	$(window).on('scroll load', function(){
-		$('live').each( function() {
-			var this_pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if ( scroll > this_pos - windowHeight ) {
-				$('.sns-link').css({
-					visibility: visible,
-					// opacity: 'translate(0)'
-				});
-			}
-		});
-	});
-});
+    $('.fade-in, .fade-left, .fade-right, .fade-up').each(function () {
+      var elementTop = $(this).offset().top;
 
-//--- FADE-IN---//
-jQuery(function($){
-	// execute when scrolling and loading
-	$(window).on('scroll load', function(){
-		$('.fade-in').each( function() {
-			var this_pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if ( scroll > this_pos - windowHeight ) {
-				$(this).css({
-					opacity: 1,
-					transform: 'translate(0)'
-				});
-			}
-		});
-	});
-});
-
-//--- FADE-IN FROM LEFT ---//
-jQuery(function($){
-	// execute when scrolling and loading
-	$(window).on('scroll load', function(){
-		$('.fade-left').each( function() {
-			var this_pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if ( scroll > this_pos - windowHeight ) {
-				$(this).css({
-					opacity: 1,
-					transform: 'translate(0)'
-				});
-			}
-		});
-	});
-});
-
-//--- FADE-IN FROM RIGHT ---//
-jQuery(function($){
-	// execute when scrolling and loading
-	$(window).on('scroll load', function(){
-		$('.fade-right').each( function() {
-			var this_pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if ( scroll > this_pos - windowHeight ) {
-				$(this).css({
-					opacity: 1,
-					transform: 'translate(0)'
-				});
-			}
-		});
-	});
-});
-
-//--- FADE-IN FROM BOTTOM ---//
-jQuery(function($){
-	// execute when scrolling and loading
-	$(window).on('scroll load', function(){
-		$('.fade-up').each( function() {
-			var this_pos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if ( scroll > this_pos - windowHeight ) {
-				$(this).css({
-					opacity: 1,
-					transform: 'translate(0)'
-				});
-			}
-		});
-	});
+      if (scrollTop > elementTop - windowHeight) {
+        $(this).css({
+          opacity: 1,
+          transform: 'translate(0)'
+        });
+      }
+    });
+  });
 });
